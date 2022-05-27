7354a37c36377a77ea94d6ed3b7771d90ffe9cbf
Shakhemir <shakhemir@gmail.com>
2022-05-28 02:05:29 +0300

    И наконец первое ДЗ

--- dz_03_01.py
+++ dz_03_01.py
@@ -1,2 +1,19 @@
 "Найти НОК двух чисел"
 
+a = int(input("Число a = "))
+b = int(input("Число b = "))
+
+
+def NOD(a, b):
+    result = 1
+    for i in range(2, min(a, b) + 1):
+        if a % i == b % i == 0:
+            result = i
+    return result
+
+
+def NOK(a, b):
+    return a * b // NOD(a, b)
+
+
+print(f'НОК({a}, {b}) = {NOK(a, b)}')
