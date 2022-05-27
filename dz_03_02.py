5320cd05af15068249b2cbdb2a39b9c0c0050375
Shakhemir <shakhemir@gmail.com>
2022-05-28 01:51:02 +0300

    Решил ДЗ 02

--- dz_03_02.py
+++ dz_03_02.py
@@ -1,1 +1,9 @@
-"Вычислить число pi c заданной точностью d"
+"""
+Вычислить число pi c заданной точностью d
+Пример: при d = 0.001,  c= 3.141.
+"""
+from math import pi
+
+d = 0.001
+tochnost = len(str(d).split('.')[1])
+print(str(pi)[:tochnost + 2])
